1477c88d210cab76d04f7a401521a6b1e58119cf
Arthur <arthur.candelaria@live.com>
2019-02-12 22:19:42 -0200

    Aula 3 - dia 2

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -13,8 +13,17 @@ class NegociacaoController {
     adiciona(event) {
         event.preventDefault();
 
-        console.log(typeof(this._inputData.value));
-        console.log(this._inputData.value);
+        let data = new Date(...
+            this._inputData.value.split('-').map((item, indice) => item - indice % 2));
+        
+        let negociacao = new Negociacao(
+            data,
+            this._inputQuantidade.value,
+            this._inputValor.value
+        )
+
+        console.log(negociacao);
+
     }
 
 }
