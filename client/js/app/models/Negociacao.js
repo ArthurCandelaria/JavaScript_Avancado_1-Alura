25be0c3f030c04df00cd824fe57ca04b94b8ebdc
Arthur <arthur.candelaria@live.com>
2019-03-06 20:04:17 -0300

    Fim aula 4

--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -1,6 +1,6 @@
 class Negociacao {
     
-    constructor(data, quantidade, valor) {        
+    constructor(data, quantidade, valor) {
         
         this._data = new Date(data.getTime());
         this._quantidade = quantidade;
@@ -14,14 +14,17 @@ class Negociacao {
     }
     
     get data() {
+        
         return new Date(this._data.getTime());
     }
     
     get quantidade() {
+        
         return this._quantidade;
     }
     
     get valor() {
+        
         return this._valor;
     }
 }
